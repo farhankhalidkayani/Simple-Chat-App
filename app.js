605056785ecfd47a2247c5a0603b5567f8b88d7c
Farhan Khalid <rj.farhan4232@gmail.com>
2024-09-22 10:24:15 +0500

    Completed the application

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const path = require("path");
+const methodOverride = require("method-override");
 const mongoose = require("mongoose");
 const Chat = require("./models/chat");
 
@@ -17,12 +18,13 @@ async function main() {
 
 const app = express();
 
-app.set(express.static("public"));
+app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(methodOverride("_method"));
 
 app.get("/", (req, res) => {
   res.render("welcome.ejs");
@@ -30,6 +32,47 @@ app.get("/", (req, res) => {
 app.get("/chats", async (req, res) => {
   const chats = await Chat.find();
   res.render("chats.ejs", { chats });
+});
+
+app.get("/chats/new", (req, res) => {
+  res.render("newForm.ejs");
+});
+
+app.get("/chats/:id/edit", async (req, res) => {
+  const { id } = req.params;
+  const chat = await Chat.findById(id);
+  res.render("editForm.ejs", { chat });
+});
+
+app.post("/chats", async (req, res) => {
+  const { from, to, message } = req.body;
+  const chat1 = new Chat({ from, to, message, createdAt: new Date() });
+  await chat1.save();
+  res.redirect("/chats");
+});
+
+app.get("/chats/:id", async (req, res) => {
+  const { id } = req.params;
+  const chat = await Chat.findById(id);
+  res.render("chat.ejs", { chat });
+});
+
+app.put("/chats/:id", async (req, res) => {
+  const { id } = req.params;
+  const { from, to, message } = req.body;
+  await Chat.findByIdAndUpdate(id, {
+    from: from,
+    to: to,
+    message: message,
+    createdAt: new Date(),
+  });
+  res.redirect("/chats");
+});
+
+app.delete("/chats/:id", async (req, res) => {
+  const { id } = req.params;
+  await Chat.findByIdAndDelete(id);
+  res.redirect("/chats");
 });
 
 app.listen(3000);
